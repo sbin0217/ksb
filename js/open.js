72025c7a9f094ef70b8cd9c1d21ad0498622e85e
sbin0217 <94364568+sbin0217@users.noreply.github.com>
2021-11-19 21:18:31 +0900

    Add files via upload

---
+++ js/open.js
@@ -0,0 +1,71 @@
+﻿ $(function(){
+
+        //1번째 이미지
+        $('.btn1').click(function(){
+          window.open("img/1.jpg","blank","width=1024, height=768")
+        });
+
+
+
+        //2번째 이미지
+        $('.btn2').click(function(){
+          window.open("img/2.jpg","blank","width=768, height=1024")
+        });
+
+
+
+
+        //3번째 이미지
+        $('.btn3').click(function(){
+          window.open("img/3.jpg","blank","width=768, height=1024")
+        });
+
+
+
+        //4번째 이미지
+        $('.btn4').click(function(){
+          window.open("img/4.jpg","blank","width=768, height=1024")
+        });
+
+
+
+
+        //5번째 이미지
+        $('.btn5').click(function(){
+          window.open("img/5.jpg","blank","width=768, height=1024")
+        });
+
+
+
+
+        //6번째 이미지
+        $('.btn6').click(function(){
+          window.open("img/6.jpg","blank","width=850, height=1200")
+        });
+
+
+
+
+
+
+        //7번째 이미지
+        $('.btn7').click(function(){
+          window.open("img/7.jpg","blank","width=850, height=1200")
+        });
+
+
+
+
+
+
+        //8번째 이미지
+        $('.btn8').click(function(){
+          window.open("img/8.jpg","blank","width=850, height=1200")
+        });
+
+
+
+        });
+
+
+
